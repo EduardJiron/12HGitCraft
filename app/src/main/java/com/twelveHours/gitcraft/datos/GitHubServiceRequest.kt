87bdcde77b3f9acc55195cddb999a7ee68a3a5fa
Eduard Jiron <eduard.jiron13901@est.uca.edu.ni>
2023-05-02 03:28:09 -0600

    documentancio

--- app/src/main/java/com/twelveHours/gitcraft/datos/GitHubServiceRequest.kt
+++ app/src/main/java/com/twelveHours/gitcraft/datos/GitHubServiceRequest.kt
@@ -6,24 +6,22 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
+//interface encargada de comunicarse con la api de github
 interface GitHubServiceRequest {
 
-
+    // peticion para obtener los datos del usuario
     @GET("users/{username}")
     fun getUser(@Path("username") username: String): Call<User>
 
 
 
-
+    // peticion para obtener los seguidores del usuario
     @GET("users/{user}/followers")
 
     fun getfollo(@Path("user") user: String): Call<List<User>>
 
+    // peticion para obtener los repositorios del usuario
+
     @GET("users/{username}/repos")
     fun getStarredRepositories(@Path("username") username: String): Call<List<Repository>>
-
-
-
-
-
 }
