c57d644892ccb009b24a8d5478b21d2b987a0346
EduardJiron <eduard.jiron13901@est.uca.edu.ni>
2023-05-12 22:27:23 -0600

    coreccion errores de merge

--- app/src/main/java/com/twelveHours/gitcraft/MainActivity.kt
+++ app/src/main/java/com/twelveHours/gitcraft/MainActivity.kt
@@ -3,10 +3,8 @@ package com.twelveHours.gitcraft
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import com.twelveHours.gitcraft.datos.GitHubServiceRequest
-import com.twelveHours.gitcraft.entidad.Repository
-
+import com.twelveHours.gitcraft.negocio.GitUserView
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -15,8 +13,18 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-            val call =GitUserView()
+        val githubApiService = retrofit.create(GitHubServiceRequest::class.java)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val username = "EduardJiron"
+
+            val call = GitUserView()
 
 
 
