8b44f019fc5f3f2735c2c5278e352d4d0b69b83d
EduardJiron <eduard.jiron13901@est.uca.edu.ni>
2023-05-13 19:17:43 -0600

    Fragment Usuario funcional,uso de cache implementado

--- app/src/main/java/com/twelveHours/gitcraft/MainActivity.kt
+++ app/src/main/java/com/twelveHours/gitcraft/MainActivity.kt
@@ -1,49 +1,24 @@
-import android.annotation.SuppressLint
+package com.twelveHours.gitcraft
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.twelveHours.gitcraft.datos.GitHubServiceRequest
-import com.twelveHours.gitcraft.datos.RepoCallback
-import com.twelveHours.gitcraft.datos.UserCallback
-import com.twelveHours.gitcraft.entidad.Repository
-import com.twelveHours.gitcraft.entidad.User
-import com.twelveHours.gitcraft.negocio.GitRepoView
-import com.twelveHours.gitcraft.negocio.GitUserView
-import com.twelveHours.gitcraft.negocio.vistas.RepoAdacterVh
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
-
-class MainActivity : AppCompatActivity(), RepoCallback, UserCallback {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: RepoAdacterVh
+class MainActivity : AppCompatActivity() /* RepoCallback, UserCallback*/ {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-        val navController = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)?.findNavController()
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView)?.findNavController()
 
         navView.setupWithNavController(navController!!)
 
-        navView.setOnNavigationItemSelectedListener { menuItem ->
+        navView.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.usuarioFragment -> {
                     navController.navigate(R.id.usuarioFragment)
@@ -61,42 +36,6 @@ class MainActivity : AppCompatActivity(), RepoCallback, UserCallback {
             }
         }
 
-        val githubApiService = Retrofit.Builder()
-            .baseUrl("https://api.github.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(GitHubServiceRequest::class.java)
 
-        val username = "EduardJiron"
-
-        val gitUserView = GitUserView()
-        gitUserView.getUser(githubApiService, username, this)
-
-        recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        val gitRepoView = GitRepoView()
-        gitRepoView.getRepos(githubApiService, username, this)
-    }
-
-    override fun onReposReceived(repos: List<Repository>) {
-        adapter = RepoAdacterVh(repos)
-        recyclerView.adapter = adapter
-    }
-
-    override fun onUserReceived(user: User) {
-        val imageView = findViewById<ImageView>(R.id.imageUser)
-        val nombreUser = findViewById<TextView>(R.id.nombreUser)
-        val seguidos = findViewById<TextView>(R.id.seguidos)
-        val seguidores = findViewById<TextView>(R.id.seguidores)
-
-        Glide.with(this).load(user.image).into(imageView)
-        nombreUser.text = user.login
-        seguidos.text = user.following
-        seguidores.text = user.followers
-    }
-
-    override fun onError(errorMessage: String) {
-        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/twelveHours/gitcraft/NuevoFragment.kt
+++ app/src/main/java/com/twelveHours/gitcraft/NuevoFragment.kt
@@ -11,11 +11,6 @@ import android.widget.TextView
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NuevoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NuevoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -28,45 +23,24 @@ class NuevoFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
 
 
-
         }
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val text: TextView = view.findViewById(R.id.nombreUser)
         text.text = "Hello"
     }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-
-
-
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
 
     ): View? {
-        // Inflate the layout for this fragment
+
 
         return inflater.inflate(R.layout.fragment_nuevo, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NuevoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NuevoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
